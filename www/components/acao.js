39ce672375fd7d5abbd8fddfe0050d23eaf2a7c4
david042 <david.mal04@hotmail.com>
2021-08-06 08:28:19 +0900

    mostra a temperatura em graus célsius, condição, umidade em porcentagem e velocidade do vento atuais

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,7 +1,11 @@
 window.onload = function(){
   const cidade = document.querySelector("#cidade");
   const botao = document.querySelector("#botao");
-  const id = document.querySelector("#id");
+  const temp = document.querySelector("#temp");
+  const description = document.querySelector("#description");
+  const city = document.querySelector("#city");
+  const humidity = document.querySelector("#humidity");
+  const wind_speedy = document.querySelector("#wind_speedy");
 
   botao.addEventListener("click", function(){
     fetch(`https://api.hgbrasil.com/weather?key=203f1c51&city_name=${cidade.value}`,{
@@ -10,8 +14,18 @@ window.onload = function(){
       cache: "default"
     }).then(response=>{
       response.json().then(data=>{
-        id.innerHTML = data['results']['city'];
+        temp.innerHTML = data['results']['temp'];
+        description.innerHTML = data['results']['description'];
+        humidity.innerHTML = data['results']['humidity'];
+        wind_speedy.innerHTML = data['results']['wind_speedy'];
       });
     });
   })
 }
+/*
+temp
+description
+city
+humidity
+wind_speedy
+*/
