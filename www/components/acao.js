296fce0c6a8294fd710593c8ee0e68846f4612c3
david042 <david.mal04@hotmail.com>
2021-08-06 09:03:46 +0900

    mostra a previsão de 5 dias com máxima, mínima e condição

--- www/components/acao.js
+++ www/components/acao.js
@@ -6,6 +6,11 @@ window.onload = function(){
   const city = document.querySelector("#city");
   const humidity = document.querySelector("#humidity");
   const wind_speedy = document.querySelector("#wind_speedy");
+  const date = [];
+  const weekday = [];
+  const max = [];
+  const min = [];
+  const prevDescription = [];
 
   botao.addEventListener("click", function(){
     fetch(`https://api.hgbrasil.com/weather?key=203f1c51&city_name=${cidade.value}`,{
@@ -18,14 +23,21 @@ window.onload = function(){
         description.innerHTML = data['results']['description'];
         humidity.innerHTML = data['results']['humidity'];
         wind_speedy.innerHTML = data['results']['wind_speedy'];
+
+        for(let i = 0; i < 5; i++){
+          date[i] = document.querySelector(`#date${i}`);
+          weekday[i] = document.querySelector(`#weekday${i}`);
+          max[i] = document.querySelector(`#max${i}`);
+          min[i] = document.querySelector(`#min${i}`);
+          prevDescription[i] = document.querySelector(`#prevDescription${i}`);
+
+          date[i].innerHTML = data['results']['forecast'][i]['date'];
+          weekday[i].innerHTML = data['results']['forecast'][i]['weekday'];
+          max[i].innerHTML = data['results']['forecast'][i]['max'];
+          min[i].innerHTML = data['results']['forecast'][i]['min'];
+          prevDescription[i].innerHTML = data['results']['forecast'][i]['description'];
+        }
       });
     });
   })
 }
-/*
-temp
-description
-city
-humidity
-wind_speedy
-*/
